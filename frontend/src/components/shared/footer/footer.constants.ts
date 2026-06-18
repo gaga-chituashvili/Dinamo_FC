8dfc236e73ab770d80777214b145f25c8b270142
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-06-18 14:09:19 +0400

    feat: add footer component

---
+++ frontend/src/components/shared/footer/footer.constants.ts
@@ -0,0 +1,37 @@
+import { NAV_ROUTES } from "../routes";
+
+export const SOCIAL_LINKS = [
+  { href: "https://dinamo.ge", label: "Website", type: "globe" },
+  {
+    href: "https://www.youtube.com/user/FCDinamoTV",
+    label: "YouTube",
+    type: "youtube",
+  },
+  {
+    href: "https://www.facebook.com/FCDinamoTbilisi1946",
+    label: "Facebook",
+    type: "facebook",
+  },
+  {
+    href: "https://www.instagram.com/fcdinamotbilisi/",
+    label: "Instagram",
+    type: "instagram",
+  },
+  { href: "https://x.com/fcdinamotbilisi", label: "X", type: "X" },
+];
+
+export const INFO_LINKS = [
+  { label: "Privacy Policy", href: NAV_ROUTES.privacy },
+  { label: "Terms of Service", href: NAV_ROUTES.terms },
+  { label: "Contact Us", href: NAV_ROUTES.contact },
+];
+
+export const FAN_ZONE_LINKS = [
+  { label: "Fan Club", href: "/fanclub" },
+  { label: "Membership", href: "/membership" },
+  { label: "Ticketing Info", href: "/tickets" },
+  { label: "Academy Registration", href: "/academy" },
+];
+
+export const DINAMO_LOGO_URL =
+  "https://erovnuliliga.ge/sites/default/files/2018-02/dinamo-tbilisi-en.png";
