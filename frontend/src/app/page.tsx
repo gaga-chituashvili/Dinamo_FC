9dd88b0ac2c64302fe6334c7cf11f804a888501e
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-06-18 14:22:37 +0400

    feat:add nextmatch component in home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "@/src/features/home/components/Hero";
+import { NextMatch } from "@/src/features/home/components/NextMatch";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#0a0a0a]">
       <Hero />
+      <NextMatch />
     </div>
   );
 }
