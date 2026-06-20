7745af727316b28c4f833641a76287ec0105d6dd
gaga-chituashvili <gagachituashvili7@gmail.com>
2026-06-20 13:08:52 +0400

    fix(squad): resolve Player type import path in squad feature components

--- frontend/src/features/squad/components/PlayerCard.tsx
+++ frontend/src/features/squad/components/PlayerCard.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import type { Player } from "../types";
+import type { Player } from "../types/squad.types";
 
 interface PlayerCardProps {
   player: Player;
--- frontend/src/features/squad/components/PositionGroup.tsx
+++ frontend/src/features/squad/components/PositionGroup.tsx
@@ -1,4 +1,4 @@
-import type { Player } from "../types";
+import type { Player } from "../types/squad.types";
 import { PlayerCard } from "./PlayerCard";
 
 interface PositionGroupProps {
